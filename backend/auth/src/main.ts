a383e9386b0fcfc78d7c67f24f3e730e6a3caa39
Monzer Mohamed <monzer.mohamed@adafsa.gov.ae>
2025-02-27 15:09:39 +0400

    add swagger

--- backend/auth/src/main.ts
+++ backend/auth/src/main.ts
@@ -1,23 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const logger = new Logger('Bootstrap');
 
 
 async function bootstrap() {
   try {
-    const app = await NestFactory.create(AppModule); 
+    const app = await NestFactory.create(AppModule);
     app.enableCors({
       origin: '*',
       methods: '*',
       allowedHeaders: 'Content-Type, Authorization',
-    }); 
-    await app.listen(4000); 
+    });
+
+    // Swagger Configuration
+    const config = new DocumentBuilder()
+      .setTitle('Auth Microservice API')
+      .setDescription('API documentation for authentication service')
+      .setVersion('1.0')
+      .addBearerAuth()  
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document);
+
+    await app.listen(4000);
     logger.log(`Auth Service running on http://localhost:4000 ...`);
 
-  } catch (error) { 
-    logger.error('internal Server Error', error); 
+  } catch (error) {
+    logger.error('internal Server Error', error);
     process.exit(1);
   }
 }
